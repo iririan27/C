f37c255b314611d792f7959337640ca0a778ac94
IririanFM <iririanscris@gmail.com>
2020-10-19 15:47:11 -0500

    Actualización
    Se agrego orde en las operaciones utilizando paréntesis, la función de potencia (pow) y la indicación de solo imprimir 2 decimales al resultado.

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -7,8 +7,8 @@ int r;
 float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
-l=2*PI*r; 
+l=((2*PI)*r); 
 a=PI*pow(r, 2);
-printf("La longitud de la circunferencia vale %0.3f\n", l);
-printf("El área del círculo vale %0.3f\n", a);
+printf("La longitud de la circunferencia vale %0.2f\n", l);
+printf("El área del círculo vale %0.2f\n", a);
 }
