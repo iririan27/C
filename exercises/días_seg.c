8c91724b2702b926190baafec7b0d5b0ddda2fcf
IririanFM <iririanscris@gmail.com>
2020-10-22 15:01:30 -0500

    elaboración de ejercicio
    se realizo un ejercicio para el cálculo de segundos en un día o días determinados

--- exercises/días_seg.c
+++ exercises/días_seg.c
@@ -1,6 +1,14 @@
 #include <studio.h>
 
 int main (){
-	int días; 
+	int dias; 
 	float resultado;  
+	
+	printf ("Ingresa el numero de dias para calcular los segundos que tiene:");
+	
+	scanf("%d", %dias);
+	resultado = dias* (24 * 60 * 60);
+	printf ("\tEn %d dias hay %02f segundos.\n", dias, resultado);
+	
+	return 0 ;
 }
