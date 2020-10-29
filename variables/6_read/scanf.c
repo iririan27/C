0ee9786ea4647afdbbd259e78ffbc2f0026f23f4
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 13:12:54 -0600

    Update scanf.c
    Se retirar paréntesis

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #define PI 3.14159
+
 int main()
 {
 int r;
 float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
-l=((2*PI)*r); 
-a=PI*pow(r, 2);
+l = (2 * PI) * r; 
+a= PI * pow(r, 2);
 printf("La longitud de la circunferencia vale %0.2f\n", l);
 printf("El área del círculo vale %0.2f\n", a);
 }
